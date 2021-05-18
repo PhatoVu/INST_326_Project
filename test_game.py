35b204456855a85ef236b9a5394af538466c3086
djhykim <81786043+djhykim@users.noreply.github.com>
2021-05-18 00:45:11 -0400

    Update test_game.py

--- test_game.py
+++ test_game.py
@@ -9,3 +9,21 @@ def test_get_results():
     assert(new_game.get_results(.4, 20)) == pytest.approx(50)
     assert(new_game.get_results(.425, 100)) == pytest.approx(235.2940)
     assert(new_game.get_results(39, 999)) == pytest.approx(25.6154)
+def test_get_rank():
+    assert(new_game.get_rank(0)) == "Newb"
+    assert(new_game.get_rank(60)) == "Newb"
+    assert(new_game.get_rank(60.2)) == "Basic"
+    assert(new_game.get_rank(80)) == "Basic"
+    assert(new_game.get_rank(80.3)) == "Speedy"
+    assert(new_game.get_rank(99)) == "Speedy"
+    assert(new_game.get_rank(100.7)) == "Flashster"
+    assert(new_game.get_rank(120)) == "Flashster"
+    assert(new_game.get_rank(120.5)) == "Master Typer"
+    assert(new_game.get_rank(140)) == "Master Typer"
+    assert(new_game.get_rank(141.2)) == "Demi-God Typer"
+    assert(new_game.get_rank(160)) == "Demi-God Typer"
+    assert(new_game.get_rank(160.434)) == "Ascended Typer"
+    assert(new_game.get_rank(179.3)) == "Ascended Typer"
+    assert(new_game.get_rank(180.2)) == "Grandmaster Typer"
+    assert(new_game.get_rank(400)) == "Grandmaster Typer"
+    assert(new_game.get_rank(400.34342)) == "Grandmaster Typer"
